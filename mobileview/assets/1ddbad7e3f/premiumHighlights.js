149cdd15ae67be8bfe8a2ba0e4e514126333199a
Dostupnyilutsk <lena.boruslav@gmail.com>
2023-04-20 18:06:12 +0200

    add a project

---
+++ mobileview/assets/1ddbad7e3f/premiumHighlights.js
@@ -0,0 +1,1 @@
+$(function(){$(".premium-highlights-detail-link").on("click",function(){return $('#premium-highlights-tab-menu a[href="#highlights"]').tab("show"),$("html, body").animate({scrollTop:$("#highlights").offset().top-100},300),!1})});
